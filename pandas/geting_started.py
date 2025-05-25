8065a7478397a2aafe9a2bbb4b3abb7acdd6d4fc
Mehul sinha <sinhamehul05@gmail.com>
2025-05-25 19:51:44 +0530

    Update geting_started.py

--- pandas/geting_started.py
+++ pandas/geting_started.py
@@ -16,15 +16,7 @@ df.tail(12)#last ki 12 values
 #sare columns ayenge
 print(df[(df.age == 48)&(df.auto_year>=2005)&(df.fraud_reported=='Y')])
 
-'''define  a new table by 
-.Dataframe({'COLUMN KA NAAM':[CONTENT AS A LIST]})
-just like dictionary'''
 
-df1=pd.DataFrame({'name':['acca' , 'bacca' , 'cacca' , 'dacca'] ,
-                 'age' :[10, 20,30,40]
-                 })
-          #.to_sring(index=False) se extra column nahi aata hai
-print(df1.to_string(index=False))
 #for specific row and column
 print(df.loc[
     (df.age == 48)&                       #cond's for rows
@@ -32,14 +24,4 @@ print(df.loc[
     (df.fraud_reported=='Y'),
     ['age','policy_bind_date','auto_year']  #comd's for column
     ])
-'''data entery by list method
-       pd.dataframes([lists],columns=[])'''
-df2=pd.DataFrame([
-    [1,'mehul','pass',98],
-    [2,'subansh','pass',78],
-    [2,'pavitra','pass',90],
-    [4,'sakashi','pass',80],
-    [5,'rudrax','fail',32]],
-    columns=['S.no.','name','status','total percentile']
-)
-print(df2.to_string(index=False))
+
