6541e3f4ce10058a047e782a65c3ab544f08c6ae
Mehul sinha <sinhamehul05@gmail.com>
2025-05-29 00:20:17 +0530

    Update 5.grouping,sort & missing val.py

--- pandas/5.grouping,sort & missing val.py
+++ pandas/5.grouping,sort & missing val.py
@@ -10,3 +10,14 @@ grp2 = grp2.rename(columns={'min': 'min amount', 'max': 'max amount'})
 print(grp2)
 mi=grp2.index
 print(type(mi))# <class 'pandas.core.indexes.multi.MultiIndex'>
+
+#   sorting 
+str=grp2.reset_index()
+# print(str)
+sot1=str.sort_values(by='min amount')
+print(sot1)
+sot2=df.sort_values(by=['auto_make','vehicle_claim'])
+print(sot2)
+''' data type and replace'''
+show=df.auto_make.dtypes
+print(show)
